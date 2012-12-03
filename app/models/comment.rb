316bf1f1edbeb384a0ef8558eb5c8d7c3fd0e865
Adam Krebs <amk528@cs.nyu.edu>
2012-12-02 22:30:59 -0500

    add the parent attr

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,4 @@
 class Comment < ActiveRecord::Base
-  attr_accessible :content, :qualification
+  attr_accessible :content, :qualification, :parent
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 end
