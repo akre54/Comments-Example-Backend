6cce3cfe31d37aa927b194717ee889a29bf3af0c
Adam Krebs <amk528@cs.nyu.edu>
2013-01-14 01:51:20 -0500

    validate presence_of :author

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ActiveRecord::Base
   attr_accessible :content, :qualification, :parent
+  validates_presence_of :author
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 end
